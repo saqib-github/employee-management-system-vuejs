400344e34c462db808aa4c68a013426ea5eaebce
saqib <rsaqib2034@gmail.com>
2021-08-09 11:13:24 +0500

    changes

--- src/views/ManageEmployees.vue
+++ src/views/ManageEmployees.vue
@@ -1,46 +1,43 @@
 <template>
   <v-container id="regular-tables" fluid tag="section">
     <h2>Manage Employees</h2>
-    <span class="material-icons-outlined"> work </span>
     <v-text-field
       label="Search"
       v-model.trim="searchValue"
-      
       :prepend-inner-icon="mdiMagnify"
     ></v-text-field>
 
-      <v-simple-table>
-        <thead>
-          <tr>
-            <th class="success--text">Company Id</th>
-            <th class="success--text">Name</th>
-            <th class="success--text">Country</th>
-            <th class="success--text">City</th>
-            <th class="success--text">Salary</th>
-            <th class="success--text">Options</th>
-          </tr>
-        </thead>
+    <v-simple-table>
+      <thead>
+        <tr>
+          <th class="success--text">Company Id</th>
+          <th class="success--text">Name</th>
+          <th class="success--text">Country</th>
+          <th class="success--text">City</th>
+          <th class="success--text">Salary</th>
+          <th class="success--text">Options</th>
+        </tr>
+      </thead>
 
-        <tbody>
-          <tr v-for="i in 10" :key="i">
-            <td>1</td>
-            <td>Dakota Rice</td>
-            <td>Niger</td>
-            <td>Oud-Turnhout</td>
-            <td class="">$36,738</td>
-            <td class="">
-              <v-btn icon color="blue">view</v-btn>
-              <v-btn icon color="red" @click="deleteEmployee"
-                ><v-icon>{{ mdiTrashCan }}</v-icon></v-btn
-              >
-              <v-btn icon color="green" @click="getAlldata"
-                ><v-icon>{{ mdiPencil }}</v-icon></v-btn
-              >
-            </td>
-          </tr>
-        </tbody>
-      </v-simple-table>
-
+      <tbody>
+        <tr v-for="data in getAlldata" :key="data._id">
+          <td>{{ data.company_id }}</td>
+          <td>{{ !data.name ? "yes" : data.name  }}</td>
+          <td>{{ data.country }}</td>
+          <td>{{ data.city }}</td>
+          <td class="">{{ data.salary }}</td>
+          <td class="">
+            <v-btn icon color="blue">view</v-btn>
+            <v-btn icon color="red" @click="deleteEmployee"
+              ><v-icon>{{ mdiTrashCan }}</v-icon></v-btn
+            >
+            <v-btn icon color="green"
+              ><v-icon>{{ mdiPencil }}</v-icon></v-btn
+            >
+          </td>
+        </tr>
+      </tbody>
+    </v-simple-table>
   </v-container>
 </template>
 
@@ -57,7 +54,7 @@ export default {
       mdiPencil: mdiPencil,
       mdiMagnify: mdiMagnify,
       searchValue: "",
-      data: [],
+      allEmployees: [],
     };
   },
   methods: {
@@ -76,10 +73,19 @@ export default {
         }
       });
     },
+  },
+  computed: {
     getAlldata() {
+      console.log("search value", this.searchValue);
+      var data = [];
+      // eslint-disable-next-line vue/no-async-in-computed-properties
       Helpers.GetAlldata("http://localhost:5000/employee").then((result) => {
-        console.log(result.data);
+        for (let i = 0; i < result.data.length; i++) {
+          data.push(result.data[i]);
+        }
       });
+      console.log(data);
+      return data;
     },
   },
 };
